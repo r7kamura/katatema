4436242f661bd7ebb0037bcf802257324b252f10
r7kamura <r7kamura@gmail.com>
2016-11-11 03:46:09 +0900

    Use webpack to build pages

--- src/build.js
+++ src/build.js
@@ -1,6 +1,37 @@
+import glob from "glob-promise";
+import webpack from "webpack";
+
 export default () => {
-  return new Promise((resolve) => {
-    console.log("build");
-    resolve();
+  return glob("./pages/**/*.js").then((paths) => {
+    const entry = {};
+    paths.forEach((path) => {
+      entry[path.replace("./pages", "")] = path;
+    });
+    return webpack({
+      entry,
+      module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            loader: "babel-loader",
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: [
+          "",
+          ".js",
+          ".jsx",
+        ],
+      },
+      output: {
+        filename: "[name]",
+        path: "./docs",
+      }
+    }).run((error, stats) => {
+      console.log(error);
+      console.log(stats);
+    });
   });
 }
