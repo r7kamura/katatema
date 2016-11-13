92156654f774257b27cc402a7b0100eb3ec46ec7
r7kamura <r7kamura@gmail.com>
2016-11-14 00:16:36 +0900

    Add router

--- src/http-server.js
+++ src/http-server.js
@@ -1,31 +1,42 @@
 import http from "http";
 import render from "./render";
+import Router from "./router";
+import send from "send";
+
+function createRouter() {
+  const router = new Router();
+  router.get("/javascripts/modan-client.js", (request, response, params) => {
+    send(request, "./docs/javascripts/modan-client.js").pipe(response);
+  });
+  router.get("/:path*", (request, response, params) => {
+    let path = request.url;
+    if (path.endsWith("/")) {
+      path += "index";
+    }
+    const html = render(`pages${path}.js`);
+    response.setHeader("Content-Type", "text/html");
+    response.setHeader("Content-Length", Buffer.byteLength(html));
+    response.end(html);
+  });
+  return router;
+}
+
+function createServer(router) {
+  return http.createServer((request, response) => {
+    router.match(request, response);
+  });
+}
 
 export default class HttpServer {
   constructor({ port }) {
     this.port = port;
-    this.server = http.createServer((request, response) => {
-      new Promise((resolve) => {
-        let path = request.url;
-        if (path.endsWith("/")) {
-          path += "index";
-        }
-        const html = render(`pages${path}.js`);
-        response.setHeader("Content-Type", "text/html");
-        response.setHeader("Content-Length", Buffer.byteLength(html));
-        response.end(html);
-        resolve();
-      }).catch((error) => {
-        console.error(error);
-        resopnse.status(500);
-        response.end("Error");
-      });
-    });
   }
 
   run() {
+    const router = createRouter();
+    const server = createServer(router);
     return new Promise((resolve, reject) => {
-      this.server.listen(this.port, (error) => {
+      server.listen(this.port, (error) => {
         if (error) {
           reject(error);
         } else {
---
+++ src/router.js
@@ -0,0 +1,38 @@
+import pathMatch from "path-match";
+import url from "url";
+
+const route = pathMatch();
+
+export default class Router {
+  constructor() {
+    this.routesMap = new Map();
+  }
+
+  addRoute(method, path, callback) {
+    const routes = this.routesMap.get(method) || new Set();
+    routes.add({
+      callback,
+      match: route(path),
+    });
+    this.routesMap.set(method, routes);
+  }
+
+  get(path, callback) {
+    this.addRoute("GET", path, callback);
+  }
+
+  match(request, response) {
+    const routes = this.routesMap.get(request.method);
+    if (routes) {
+      const { pathname } = url.parse(request.url);
+      routes.forEach(({ callback, match }) => {
+        const params = match(pathname);
+        if (params) {
+          return new Promise((resolve) => {
+            callback(request, response, params);
+          });
+        }
+      });
+    }
+  }
+}
